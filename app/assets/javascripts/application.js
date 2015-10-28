f9c585048152e8a269ae3d511642ece5fd03255f
Justin Raines <justin.magtheridon@gmail.com>
2015-10-28 14:38:12 -0400

    upgrade google analytics code

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,6 +16,13 @@
 //= require rocky
 
 
+$('a').on('click', function(e){
+  ga('send', 'vote.usa.gov','link clicked', $(this).attr('href'));
+});
+
+$('button').on('click', function(e){
+  ga('send', 'vote.usa.gov','form submitted', window.location.href);
+});
 /**
  * Accordion
  *
